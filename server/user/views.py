18f6e948edd1da905ebaff1ba633f993edbfc108
anastassow <127148449+anastassow@users.noreply.github.com>
2024-11-23 22:25:07 +0200

    fixx

--- server/user/views.py
+++ server/user/views.py
@@ -94,13 +94,9 @@ def login(request):
     }, status=status.HTTP_200_OK)
 
 @api_view(['POST', 'GET'])
-@permission_classes([IsAuthenticated])
+#@permission_classes([IsAuthenticated])
 def user(request, id=None):
-    if request.method == 'POST':
-        users = CustomUser.objects.values('id', 'username', 'first_name', 'last_name', 'email', 'age', 'gender')
-        return Response(list(users))
-    
-    if request.method == 'GET':
+    if request.method == 'GET' and id is not None:
         user = get_object_or_404(CustomUser, pk=id)
         return Response({
         "user": {
@@ -113,3 +109,7 @@ def user(request, id=None):
             "gender": user.gender
         }
     }, status=status.HTTP_200_OK)
+
+    if request.method == 'GET' and id is None:
+        users = CustomUser.objects.values('id', 'username', 'first_name', 'last_name', 'email', 'age', 'gender')
+        return Response(list(users))
